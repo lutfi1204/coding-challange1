a1cff68d7ba2df211f93bae76b8e9793115b30c3
lutfi1204 <144406159+lutfi1204@users.noreply.github.com>
2023-10-05 21:40:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+    //input angka yang sudah di berikan yang nantinya akan di jadikan pembatas
+    let inputNumber = 6;
+
+    //perulangan untuk menampilkan 
+    for(let i = 1; i <= inputNumber; i++){
+        let currentCube = i * i * i;
+
+        console.log(`Current number is: ${i} and the cube is ${currentCube}`);
+    }
+
+    
